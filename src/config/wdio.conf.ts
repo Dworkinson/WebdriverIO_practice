53438a9bc3720f5730508a60d1b8196d84f8e9b6
dworkinson <andrii.svynarov@gmail.com>
2026-04-13 09:11:46 +0300

    initial commit

---
+++ src/config/wdio.conf.ts
@@ -0,0 +1,42 @@
+import type { Options } from '@wdio/types'
+export const config: Options.Testrunner = {
+    runner: 'local',
+    autoCompileOpts: {
+        autoCompile: true,
+        tsNodeOpts: {
+            project: './tsconfig.json',
+            transpileOnly: true
+        }
+    },
+    rootDir: ".",
+    specs: [
+        './src/specs/**/*.spec.ts'
+    ],
+    exclude: [
+    ],
+    maxInstances: 10,
+    capabilities: [{
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: [
+                '--disable-background-networking',
+                '--disable-sync',
+                '--disable-extensions'
+            ],
+        }
+    }],
+
+    logLevel: 'error',
+    bail: 0,
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    framework: 'mocha',
+    
+    reporters: ['spec'],
+
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+}
