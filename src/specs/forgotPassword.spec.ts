d1ce192181d9048be0abc42c2f5cb631176999c4
dworkinson <andrii.svynarov@gmail.com>
2026-04-29 14:34:13 +0300

    feat: forgot password spec added.

---
+++ src/specs/forgotPassword.spec.ts
@@ -0,0 +1,52 @@
+import { browser } from "@wdio/globals";
+import { expect } from "chai";
+
+import ForgotPasswordPage from "@pages/forgotPasswordPage/forgotPassword.page";
+import { deleteAllMessages, getLatestMessageText } from "@helpers/gmail/getMailContent";
+import { waitForResult } from "@helpers/waitForFunctionResult";
+import * as user from "@data/sensetive/mail.json";
+
+const TOKEN_PATH = 'src/data/sensetive/token.json';
+const urlRegExp = new RegExp(/https:\/\/\S*\/login/);
+
+
+function getRedirectUrl(regExp: RegExp, msg: string) {
+    const url = msg.match(regExp);
+    if (url) {
+        return url[0];
+    }
+}
+
+describe('Forgot Password', async () => {
+    before(async () => {
+        const regExp = new RegExp('.*ads.*');
+        (await browser.mock(regExp)).abort('Aborted');
+
+        await ForgotPasswordPage.open();
+        await deleteAllMessages(TOKEN_PATH);
+    });
+
+    it('link should be received', async () => {
+        await ForgotPasswordPage.forgotPassword(user.email);
+
+        expect(await ForgotPasswordPage.isConfirmationAlertDisplayed()).to.be.true;
+        const msg = await waitForResult(getLatestMessageText, [TOKEN_PATH]);
+        const url = getRedirectUrl(urlRegExp, msg);
+
+        // in live conditions should get real url
+        // -> open url in browser
+        // -> pass restoring password process
+        // -> login with new password
+        expect(url).to.be.exist;
+    });
+
+    //delete all messages after each successful test
+    afterEach(async () => {
+        await deleteAllMessages(TOKEN_PATH);
+    })
+});
+
+//delete all messages after each suite (need in case of failure in any test)
+afterEach(async () => {
+    await deleteAllMessages(TOKEN_PATH);
+});
