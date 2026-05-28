14d6d5154d81ddddff454c7378b2ef5873390fe8
Dworkinson <andrii.svynarov@gmail.com>
2026-05-28 12:07:31 +0300

    feat(horizontal slider)[DRAFT]: added slider page with min, max, step values cache.

---
+++ src/pages/horizontalSliderPage/horizontalSlider.page.ts
@@ -0,0 +1,109 @@
+import { $, browser } from '@wdio/globals';
+import { Key } from 'webdriverio';
+
+type Direction = 'left' | 'right';
+
+type SliderConfig = {
+    min: number;
+    max: number;
+    step: number;
+};
+
+class HorizontalSliderPage {
+    // creating sliderConfig property to cache slider config and prevent unnecessary calls to its selector
+    private sliderConfig?: SliderConfig;
+
+    async open(): Promise<void> {
+        await browser.url('/horizontal-slider');
+    }
+
+    private get range(): ChainablePromiseElement {
+        return $('[id="range"]');
+    }
+
+    private get slider(): ChainablePromiseElement {
+        return $('input[type="range"]');
+    }
+
+    private async getSliderConfig(): Promise<SliderConfig> {
+        if (this.sliderConfig) {
+            return this.sliderConfig;
+        }
+
+        const min = await this.slider.getAttribute('min');
+        if (!min) throw new Error('Could not get min value from range');
+
+        const max = await this.slider.getAttribute('max');
+        if (!max) throw new Error('Could not get max value from range');
+
+        const step = await this.slider.getAttribute('step');
+        if (!step) throw new Error('Could not get step value from range');
+
+        this.sliderConfig = {
+            min: Number(min),
+            max: Number(max),
+            step: Number(step),
+        };
+
+        return this.sliderConfig;
+    }
+
+    async getRangeValue(): Promise<number> {
+        await this.range.waitForDisplayed();
+        return Number(await this.range.getText());
+    }
+
+    private async moveSliderViaKeyboard(direction: Direction): Promise<void> {
+        await this.slider.waitForDisplayed();
+
+        await browser.execute((slider) => {
+            slider.focus();
+        }, await this.slider);
+
+        switch (direction) {
+            case 'left':
+                await browser.keys(Key.ArrowLeft);
+                return;
+
+            case 'right':
+                await browser.keys(Key.ArrowRight);
+                return;
+        }
+    }
+
+    private async positionValidator(position: number): Promise<void> {
+        const { min, max, step } = await this.getSliderConfig();
+
+        if (position < min || position > max) {
+            throw new Error('Position is out of range');
+        }
+
+        if (position % step !== 0) {
+            throw new Error(`Position must be a multiple of step (step == ${step})`);
+        }
+    }
+
+    async moveSliderToPosition(position: number): Promise<void> {
+        await this.positionValidator(position);
+
+        let currentPosition = await this.getRangeValue();
+
+        if (currentPosition < position) {
+            while (currentPosition < position) {
+                await this.moveSliderViaKeyboard('right');
+                currentPosition = await this.getRangeValue();
+            }
+            return;
+        }
+
+        if (currentPosition > position) {
+            while (currentPosition > position) {
+                await this.moveSliderViaKeyboard('left');
+                currentPosition = await this.getRangeValue();
+            }
+            return;
+        }
+    }
+}
+
+export default new HorizontalSliderPage();
